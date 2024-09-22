f75c19dcb98e503c24f247c985fe2ed4e0d47520
Sulthan <sulthansrs@gmail.com>
2024-09-22 20:45:00 +0530

    Update KafkaProducer.java

--- spring-kafka/src/main/java/com/kafka/service/KafkaProducer.java
+++ spring-kafka/src/main/java/com/kafka/service/KafkaProducer.java
@@ -1,11 +1,16 @@
 package com.kafka.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducer {
+        
+	private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
+	
 	private static final String TOPIC = "test-syed";
 
 	@Autowired
