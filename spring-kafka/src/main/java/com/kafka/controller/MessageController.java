043851b12205a12e4334b4bb91b71abb502fd065
Sulthan <sulthansrs@gmail.com>
2024-09-22 20:45:36 +0530

    Update MessageController.java

--- spring-kafka/src/main/java/com/kafka/controller/MessageController.java
+++ spring-kafka/src/main/java/com/kafka/controller/MessageController.java
@@ -1,6 +1,7 @@
 package com.kafka.controller;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,11 +11,15 @@ import com.kafka.service.KafkaProducer;
 
 @RestController
 public class MessageController {
+
+	private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
+	
 	@Autowired
 	private KafkaProducer kafkaProducer;
 
 	@PostMapping("/send")
 	public void sendMessage(@RequestBody String message) {
+		logger.info("Send message : ", message);
 		kafkaProducer.sendMessage(message);
 	}
 }
